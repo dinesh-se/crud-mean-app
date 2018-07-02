0cceb9f0c030034da13ed3798dd4dd7b82dcb262
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2018-07-02 22:26:50 +0530

    added unit test cases for customer list and customer details component

--- src/app/customer/customer.component.spec.ts
+++ src/app/customer/customer.component.spec.ts
@@ -1,19 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
+import { RouterLinkStubDirective } from '../testing-stub/router-stubs';
 import { CustomerComponent } from './customer.component';
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
 
 describe('CustomerComponent', () => {
   let component: CustomerComponent;
   let fixture: ComponentFixture<CustomerComponent>;
+  let api: ApiService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomerComponent ]
+      declarations: [ CustomerComponent, RouterLinkStubDirective ],
+      providers: [ ApiService, HttpClient, HttpHandler ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    api = TestBed.get(ApiService);
     fixture = TestBed.createComponent(CustomerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +28,18 @@ describe('CustomerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#ngOnInit should call getCustomers method in ApiService', () => {
+    let customers = [
+      {
+        name: {
+          first: "test"
+        }
+      }
+    ];
+    spyOn(api, 'getCustomers').and.returnValue(Observable.create(customers));
+    component.ngOnInit();
+    expect(api.getCustomers).toHaveBeenCalled();
   });
 });
